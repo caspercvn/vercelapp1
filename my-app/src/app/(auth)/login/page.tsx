254d12f543f437c14ca841a08b9e7853a95df065
caspercvn <66311087+caspercvn@users.noreply.github.com>
2024-07-04 22:01:19 +0200

    first commit

---
+++ my-app/src/app/(auth)/login/page.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+export const metadata = {
+    title: "Login to your account",
+}
+
+export default function Login() {
+    return (
+        <>
+        <html data-theme="forest">
+    <body className="h-full">
+
+    </body>
+      </html>
+    </>
+    )
+}
+
---
+++ my-app/src/app/dashboard/page.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import Link from "next/link";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+
+export default function Dashboard() {
+    return (
+        <>
+<html data-theme="dim">
+  <body className="">
+  <header className="bg-base-100 fixed z-50 w-full">
+  <nav className="container flex items-center justify-between px-8 py-4 mx-auto max-w-7xl">
+  <details className="dropdown">
+  <summary className="btn m-1">Min Profil</summary>
+  <ul className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+    <li><a>Item 1</a></li>
+    <li><a>Item 2</a></li>
+  </ul>
+</details>
+<div>
+<button className="btn m-1">Feedback</button>
+      </div>
+  </nav>
+</header>
+<main>
+  <div className="h-full">
+  <section>
+    <div className="px-8 py-4 mx-auto max-w-7xl lg:py-32 ">
+        
+        <h1 className="mb-4 text-2xl font-bold">Dashboard</h1>
+       
+        </div> 
+        </section>
+    </div>
+</main>
+
+</body>
+</html>
+
+
+
+</>
+    )
+}

3f85c89b8f1f963823d775d56fd65188b2d6c9ec
caspercvn <66311087+caspercvn@users.noreply.github.com>
2024-07-06 16:02:01 +0200

    s

--- my-app/src/app/dashboard/page.tsx
+++ my-app/src/app/dashboard/page.tsx
@@ -25,21 +25,14 @@ export default function Dashboard() {
   <body>
 <div className=' mx-auto pt-6 pb-0 max-w-6xl'>
     
-    <header className="flex justify-between gap-4 mb-8 md:mb-16"><Link href="/logind"><button className="btn btn-m">Log ind</button></Link><Link href="/logind"><button className="btn btn-m">Log ind</button></Link></header>
+    <header className="flex justify-between gap-4 mb-8 md:mb-16"><Link href="/logind"><button className="btn btn-m">Min Profil</button></Link><Link href="/logind"><button className="btn btn-m">Feedback</button></Link></header>
     </div>
-    <main className="min-h-screen mx-auto max-lg:px-8 py-12 pb-24 max-w-6xl "><section className=" grid grid-cols-6 gap-12"><div className="col-span-full md:col-span-2"><div className="border shadow-sm	 p-8 rounded-box bg-base-100 min-h-0"><form className="space-y-8 w-full"><h2 className="font-bold text-lg">Build features users <span className="bg-neutral text-neutral-content px-1.5">really</span> want</h2><div className="form-control w-full"><div className="label"><span className="label-text">Board Name</span></div><input  type="text"  placeholder="Future Unicorn Inc. 🦄" className="input input-bordered w-full placeholder:opacity-60" value=""></input></div><div className="space-y-2"><button className="btn btn-neutral group w-full" type="submit">Create Board</button></div></form></div></div><div className="space-y-6 col-span-full md:col-span-4"><h1 className="font-extrabold text-lg md:text-xl">1 Boards</h1><ul className="grid md:grid-cols-2 gap-4"><li><a className="border shadow-sm	rounded-box block p-6 bg-base-100 hover:bg-neutral hover:text-neutral-content duration-150" href="/dashboard/666aeae1205dbbf1617def3e"><div className="space-y-2"><p className="font-bold">test111</p></div></a></li></ul></div></section>
-    
-    
-    <div>
-      <h1>Create a new Board</h1>
-      <input
+    <main className="min-h-screen mx-auto max-lg:px-8 py-12 pb-24 max-w-6xl "><section className=" grid grid-cols-6 gap-12"><div className="col-span-full md:col-span-2"><div className="border shadow-sm	 p-8 rounded-box bg-base-100 min-h-0 space-y-8 w-full"><h2 className="font-bold text-lg">Build features users <span className="bg-neutral text-neutral-content px-1.5">really</span> want</h2><div className="form-control w-full"><div className="label"><span className="label-text">Board Name</span></div><input className='input input-bordered w-full placeholder:opacity-60'
         type="text"
         value={title}
         onChange={(e) => setTitle(e.target.value)}
         placeholder="Board Title"
-      />
-      <button onClick={handleCreateBoard}>Create Board</button>
-    </div>
+      /></div><div className="space-y-2"><button className='btn btn-neutral group w-full' onClick={handleCreateBoard}>Create Board</button></div></div></div><div className="space-y-6 col-span-full md:col-span-4"><h1 className="font-extrabold text-lg md:text-xl">1 Boards</h1><ul className="grid md:grid-cols-2 gap-4"><li><a className="border shadow-sm	rounded-box block p-6 bg-base-100 hover:bg-neutral hover:text-neutral-content duration-150" href="/dashboard/666aeae1205dbbf1617def3e"><div className="space-y-2"><p className="font-bold">test111</p></div></a></li></ul></div></section>
     
     </main>
     </body>
